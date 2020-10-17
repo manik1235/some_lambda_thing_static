04229b2e3e91ed277d5513ce1645e0330ccbdb6e
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2020-10-17 13:42:31 -0400

    Add JS, update sync script
    This commit adds some basic JS and updates the sync script to include
    that file, too.

--- script/sync-to-s3.sh
+++ script/sync-to-s3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync web s3://some-lambda-thing --acl public-read --exclude "*" --include index.html --include error.html
+aws s3 sync web s3://some-lambda-thing --acl public-read --exclude "*" --include "*.html" --include "*.js"
